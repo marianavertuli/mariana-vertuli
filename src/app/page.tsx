7083f704e1f4101d9f5a9d6e7f062aba559aeae9
Mariana Vertuli <mariana.vt.s@gmail.com>
2025-09-03 14:23:14 +0400

    adjust header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { AboutMe } from "./components/about-me";
 import { Projects } from "./components/projects";
 import { Contact } from "./components/contact";
 import { getHomeData } from "@/utils/getHomeData";
+import styles from './styles.module.scss'
 
 export default async function Home() {
 
@@ -9,7 +10,7 @@ export default async function Home() {
   const details = data.objects[0].metadata;
 
   return (
-      <main style={{paddingTop: '60px'}}>
+      <main className={styles.main}>
         <AboutMe about={details.aboutme} contacts={details.contacts}/>
         <Projects projects={details.projects}/>
         <Contact contacts={details.contacts}/>
